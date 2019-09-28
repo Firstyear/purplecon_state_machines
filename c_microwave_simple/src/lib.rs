49605d822dc319627c60f45e449cc1a98ccb170e
William Brown <william@blackhats.net.au>
2019-09-28 12:51:28 +1000

    Add graffle and lib

--- c_microwave_simple/src/lib.rs
+++ c_microwave_simple/src/lib.rs
@@ -1,20 +1,61 @@
+use microwave_common::MicrowaveOps;
+
 extern {
     fn test_microwave() -> isize;
 }
 
-fn run_test() -> isize {
-    unsafe {
-        test_microwave()
+// Basically a typed void pointer.
+struct MicrowaveC {}
+
+struct Microwave {}
+
+impl MicrowaveOps for Microwave {
+    fn new() -> Self {
+        Microwave {}
+    }
+
+    fn reset(&mut self) {
+    }
+
+    fn tick(&mut self) {
+    }
+
+    fn magnetron_enabled(&self) -> bool {
+        false
+    }
+
+    fn door_open(&self) -> bool {
+        false
+    }
+
+    fn time_remain(&self) -> usize {
+        0
+    }
+
+    fn action_open_door(&mut self) {
+    }
+
+    fn action_close_door(&mut self) {
+    }
+
+    fn action_set_time(&mut self, t: usize) {
+    }
+
+    fn action_start(&mut self) {
+    }
+
+    fn action_stop(&mut self) {
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::run_test;
+    use crate::Microwave;
+    use microwave_common::{MicrowaveOps, test_microwave};
 
     #[test]
     fn it_works() {
-        let x = run_test();
-        assert_eq!(x, 1);
+        let mut mw = Microwave::new();
+        assert!(test_microwave(&mut mw));
     }
 }
